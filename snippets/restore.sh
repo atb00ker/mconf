b02b6178017b053f9235248bface16da6235e7b2
Ajay Tripathi <ajay39in@gmail.com>
2023-04-16 03:34:00 +0530

    [0.4.1] Modernised system

--- snippets/restore.sh
+++ snippets/restore.sh
@@ -5,19 +5,25 @@ if [ "$EUID" -eq 0 ]; then
     exit 1
 fi
 
-su root -c 'apt update'
-su root -c 'apt install curl git python3-pip'
-su root -c 'pip3 install ansible'
+# su root -c 'apt update'
+# su root -c 'apt install curl git python3-pip ansible'
 
-export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin
+read -p "Do you want to clone at location (~/Documents/develop/mconf)? (Y/n)" clone
 
-mkdir -p ~/Documents/develop/
-cd ~/Documents/develop/
-git clone https://github.com/atb00ker/mconf || true
-cd mconf/
-
-read -p "Enter tags you want to run: " tags
+if [[ "$clone" == [Yy]* ]] ; then
+    mkdir -p ~/Documents/develop/
+    cd ~/Documents/develop/
+    git clone https://github.com/atb00ker/mconf || true
+    cd mconf/
+fi
 
 ansible-galaxy install -r requirements.yml
 sed -i 's/role:.*/role: system-restore/' playbook.yml
-ansible-playbook -i inventory.yml playbook.yml -K --become-method su --tags "${tags}"
+
+read -p "Ready to run the playbook? (Y/n)" runPlaybook
+
+if [[ "$runPlaybook" == [Yy]* ]] ; then
+    read -p "Enter tags you want to run: " tags
+    ansible-playbook -i inventory.yml playbook.yml -K --become-method su --tags "${tags}"
+fi
+
