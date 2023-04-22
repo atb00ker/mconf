52ddf5b2749ecea74523f943df7aeae9d4fb366a
Ajay Tripathi <ajay39in@gmail.com>
2023-04-22 17:45:32 +0530

    [refactor] moved away from xorg and simplified raspi

--- snippets/restore.sh
+++ snippets/restore.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# curl -L -s http://bit.do/mconf-debian -o setup.sh; bash setup.sh
+# curl -L -s http://rebrandly.do/mconf-debian -o setup.sh; bash setup.sh
 if [ "$EUID" -eq 0 ]; then
     echo "Please do not run as root"
     exit 1
 fi
 
-# su root -c 'apt update'
-# su root -c 'apt install curl git python3-pip ansible'
+su root -c 'apt update'
+su root -c 'apt install curl git python3-pip ansible'
 
-read -p "Do you want to clone at location (~/Documents/develop/mconf)? (Y/n)" clone
+read -p "Do you want to clone at location (~/Documents/develop/mconf)? (Y/n) " clone
 
 if [[ "$clone" == [Yy]* ]] ; then
     mkdir -p ~/Documents/develop/
@@ -17,13 +17,14 @@ if [[ "$clone" == [Yy]* ]] ; then
     cd mconf/
 fi
 
+cd ~/Documents/develop/mconf/
 ansible-galaxy install -r requirements.yml
 sed -i 's/role:.*/role: system-restore/' playbook.yml
 
-read -p "Ready to run the playbook? (Y/n)" runPlaybook
+read -p "Ready to run the playbook? (Y/n) " runPlaybook
 
 if [[ "$runPlaybook" == [Yy]* ]] ; then
-    read -p "Enter tags you want to run: " tags
+    read -p "Enter tags you want to run(i3wm,xfce,raspi): " tags
     ansible-playbook -i inventory.yml playbook.yml -K --become-method su --tags "${tags}"
 fi
 
